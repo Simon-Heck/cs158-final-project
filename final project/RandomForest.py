a79edc101ab4ebf50295132f73ebbb2b03639b48
Simon-Heck <simonpheck@gmail.com>
2022-04-25 21:20:05 -0700

    second commit Co-authored-by: nmarsano <nmarsano@users.noreply.github.com> Co-authored-by: marghi2000 <marghi2000@users.noreply.github.com> Co-authored-by: Stefanos-stk <Stefanos-stk@users.noreply.github.com>

--- final project/RandomForest.py
+++ final project/RandomForest.py
@@ -1,10 +1,58 @@
-from sklearn.ensemble import RandomForestClassifier
+from random import Random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier 
 from sklearn.datasets import make_classification
 
-def main():
-    print("blah")
-    
 
+
+_clf = RandomForestClassifier(max_depth=2, random_state=0)
+
+def main():
+    RandomForestClassifier.
+
+    setParameters()
+    data = loadDataset()
+    preProcessData(data)
+
+    X, y = make_classification(n_samples=1000, n_features=4, n_informative=2, n_redundant=0, random_state=0, shuffle=False)
+
+    X, y = 
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(X, y)
+    
+    # evaluate the model
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+    n_scores = cross_val_score(clf, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+    # report performance
+    print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
+    
+    
+    
+def setParameters(clf: RandomForestClassifier):
+    clf.set_params()
+    pass
+
+def preProcessData(data):
+    scaler = preprocessing.StandardScaler().fit(data)
+
+def hyperTune(): #?
+    pass
+
+def loadModel():
+    pass
+
+def loadDataset(file_name):
+    return pd.read_csv(file_name) 
 
 if __name__ == "main":
-    main
+    main()
